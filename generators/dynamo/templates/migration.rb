8c5a4f2a717a88bd082d95b595dd55a64e5dd73b
jacklin10 <jacklin10@gmail.com>
2010-08-17 21:43:38 -0400

    Added a basic README and fixed the migration

--- generators/dynamo/templates/migration.rb
+++ generators/dynamo/templates/migration.rb
@@ -2,26 +2,27 @@
 # ./script/generate dynamo <option> and it will create the migration for you, and move it to your app's migrate directory
 class DynamoMigration < ActiveRecord::Migration
   def self.up
-    create_table :dynamo_field do |t|
+    create_table :dynamo_fields do |t|
       t.column :model, :string
       t.column :field_name, :string
       t.column :field_type, :string
       t.timestamps
     end
     
-    create_table :dynamo_field_value do |t|
+    create_table :dynamo_field_values do |t|
       t.column :dynamo_field_id, :integer
+      t.column :model_id, :integer
       t.column :val_string, :string
       t.column :val_int, :integer
       t.column :val_float, :float
       t.timestamps
     end
     
-    add_index :dynamo_field_value, :dynamo_field_id
+    add_index :dynamo_field_values, :dynamo_field_id
   end
   
   def self.down
-    drop_table :dynamo_field
-    drop_table :dynamo_field_value
+    drop_table :dynamo_fields
+    drop_table :dynamo_field_values
   end
 end
